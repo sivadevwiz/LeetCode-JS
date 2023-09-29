8435c01b794e0794aa461b19a57b2bf911cf321a
SivaDevWiz <siva.devwiz@gmail.com>
2023-09-30 01:01:02 +0530

    adding solution

---
+++ DSA_JS/src/0069_Sqrt.js
@@ -0,0 +1,43 @@
+// 69 SQRT(X) - Easy
+
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+// Constraints:
+
+// 0 <= x <= 231 - 1
+
+// Solution:
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  if ((x === 0) | (x === 1)) {
+    return x;
+  }
+
+  for (i = 2; i <= x; i++) {
+    if (i * i === x) {
+      return i;
+    }
+    if (i * i > x) {
+      return i - 1;
+    }
+  }
+};
